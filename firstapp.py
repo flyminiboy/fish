64b0b697bb4b66b536b2e7eabbf1414914eff680
fly <guopengfei@axzt.net>
2023-02-10 10:54:03 +0800

    测试chatGPT api

--- firstapp.py
+++ firstapp.py
@@ -16,21 +16,21 @@ i = st.text_input(label='输入问题', max_chars=30, value='', help='把你想�
 
 
 # Load your API key from an environment variable or secret management service
-openai.api_key = os.getenv("ENV_OPENAPI_KEY")
+# openai.api_key = os.getenv("ENV_OPENAPI_KEY")
 
-response = openai.Completion.create(model="text-davinci-003",
-  prompt=i,
-  temperature=0, 
-  max_tokens=1500,
-  top_p=1.0,
-  frequency_penalty=0.5,
-  presence_penalty=0.0)
+# response = openai.Completion.create(model="text-davinci-003",
+#   prompt=i,
+#   temperature=0, 
+#   max_tokens=1500,
+#   top_p=1.0,
+#   frequency_penalty=0.5,
+#   presence_penalty=0.0)
 
 
-st.write(response.choices[0].text)
+# st.write(response.choices[0].text)
 
-st.write("我是分割线----------")
-st.json(response)
+# st.write("我是分割线----------")
+# st.json(response)
 
 # 图表
 
